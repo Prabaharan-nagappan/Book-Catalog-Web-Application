12e7c6a1dded0544a6c0d5a75444f44614cc39b3
Prabaharan <prabaharan.nagappan@gmail.com>
2024-03-10 09:29:13 +0530

    details for books

--- core/books/urls.py
+++ core/books/urls.py
@@ -4,4 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name="books"),
+    path('book/<str:book_id>/', views.book_details, name='book_details'),
+
 ]
--- core/books/views.py
+++ core/books/views.py
@@ -22,3 +22,23 @@ def home(request):
         # If the request was not successful, handle the error accordingly
         error_message = f"Failed to fetch data from the Google Books API. Status code: {response.status_code}"
         return render(request, 'books/home.html', {'error_message': error_message})
+
+
+def book_details(request, book_id):
+    # Construct the URL for fetching details of the book using its ID
+    api_url = f'https://www.googleapis.com/books/v1/volumes/{book_id}'
+    
+    # Make a request to the Google Books API
+    response = requests.get(api_url)
+    
+    # Check if the request was successful
+    if response.status_code == 200:
+        # Parse the JSON response
+        book_data = response.json()
+        
+        # Render the details template with the book data
+        return render(request, 'books/details.html', {'book': book_data})
+    else:
+        # If the request was not successful, handle the error accordingly
+        error_message = f"Failed to fetch book details from the Google Books API. Status code: {response.status_code}"
+        return render(request, 'books/error.html', {'error_message': error_message})
