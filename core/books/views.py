624e96922a6c588eb4d987abfea74275a33b16f7
Prabaharan <prabaharan.nagappan@gmail.com>
2024-03-10 09:18:51 +0530

    books app

---
+++ core/books/views.py
@@ -0,0 +1,24 @@
+import requests
+from django.shortcuts import render
+
+def home(request):
+    query = request.GET.get('q', 'science')  # Default query is 'science' if not provided
+    api_url = f'https://www.googleapis.com/books/v1/volumes?q={query}'
+    
+    # Make a request to the Google Books API
+    response = requests.get(api_url)
+    
+    # Check if the request was successful
+    if response.status_code == 200:
+        # Parse the JSON response
+        data = response.json()
+        
+        # Extract relevant information from the response
+        books = data.get('items', [])
+        
+        # Render the template with the received data
+        return render(request, 'books/home.html', {'books': books})
+    else:
+        # If the request was not successful, handle the error accordingly
+        error_message = f"Failed to fetch data from the Google Books API. Status code: {response.status_code}"
+        return render(request, 'books/home.html', {'error_message': error_message})
--- core/core/urls.py
+++ core/core/urls.py
@@ -16,17 +16,18 @@ Including another URLconf
 """
 # Import necessary modules
 from django.contrib import admin # Django admin module
-from django.urls import path	 # URL routing
+from django.urls import path, include	 # URL routing
 from authentication.views import * # Import views from the authentication app
 from django.conf import settings # Application settings
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns # Static files serving
 
 # Define URL patterns
 urlpatterns = [
-	path('home/', home, name="recipes"),	 # Home page
+	path('home/', home, name="home"),	 # Home page
 	path("admin/", admin.site.urls),		 # Admin interface
 	path('login/', login_page, name='login_page'), # Login page
 	path('register/', register_page, name='register'), # Registration page
+    path("books/", include("books.urls"), name="books"), # Books module
 ]
 
 # Serve media files if DEBUG is True (development mode)
